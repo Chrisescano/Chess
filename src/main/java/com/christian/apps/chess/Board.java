898ccdaf84ce9d18109efc5199565f7fd548fc50
Escano, Christian <escanochristian21@gmail.com>
2025-09-27 09:33:57 -0400

    implemented castling feature

--- src/main/java/com/christian/apps/chess/Board.java
+++ src/main/java/com/christian/apps/chess/Board.java
@@ -1,7 +1,11 @@
 package com.christian.apps.chess;
 
+import com.christian.apps.piece.Piece;
+import com.christian.apps.piece.PieceType;
 import com.christian.apps.util.Position;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Board {
 
@@ -11,28 +15,61 @@ public class Board {
   private static final int BOARD_HEIGHT = 8;
 
   private final char[][] board;
+  private final List<Piece> pieces;
 
   public Board() {
     board = new char[BOARD_HEIGHT][BOARD_WIDTH];
+    pieces = new ArrayList<>();
   }
 
   /*-- Methods --*/
+
+  public Stream<Piece> searchFor(final Boolean isWhite) {
+    Stream<Piece> pieceStream = pieces.stream();
+
+    if (isWhite != null) {
+      pieceStream = pieceStream.filter(piece -> piece.isWhite() == isWhite);
+    }
+    return pieceStream;
+  }
 
   public static boolean isOutOfBounds(final Position position) {
     return position.getX() < 0 || position.getX() > 7 ||
         position.getY() < 0 && position.getY() > 7;
   }
 
-  public void placeSymbolAt(final Position position, final char symbol) {
-    board[position.getY()][position.getX()] = symbol;
+  public void registerPiece(final Piece piece) {
+    pieces.add(piece);
   }
 
-  public void removeSymbolAt(final Position position) {
-    board[position.getY()][position.getX()] = EMPTY_TILE;
+  public void unregisterPiece(final Piece piece) {
+    pieces.remove(piece);
+  }
+
+  public boolean movePiece(final Piece piece, final Position destination) {
+    final Position piecePos = piece.getPosition();
+    final char pieceSym = piece.getSymbol();
+    final char boardSym = getTile(piecePos);
+
+    if (pieceSym == boardSym) {
+      removeSymbolAt(piecePos);
+      placeSymbolAt(destination, pieceSym);
+      piecePos.setTo(destination);
+      return true;
+    }
+    return false;
   }
 
   public char getTile(final Position position) {
     return board[position.getY()][position.getX()];
+  }
+
+  public Piece getPiece(final Position position) {
+    try {
+      return pieces.stream().filter(piece -> piece.getPosition().equals(position)).toList().getFirst();
+    } catch (Exception ignored) {
+    }
+    return null;
   }
 
   public char[] getRangeOfTiles(final List<Position> path) {
@@ -44,4 +81,12 @@ public class Board {
   }
 
   /*-- Helper Methods --*/
+
+  private void placeSymbolAt(final Position position, final char symbol) {
+    board[position.getY()][position.getX()] = symbol;
+  }
+
+  private void removeSymbolAt(final Position position) {
+    board[position.getY()][position.getX()] = EMPTY_TILE;
+  }
 }
--- src/main/java/com/christian/apps/piece/Rook.java
+++ src/main/java/com/christian/apps/piece/Rook.java
@@ -7,6 +7,10 @@ public class Rook extends Piece {
 
   public static final char WHITE_SYMBOL = 'R';
   public static final char BLACK_SYMBOL = 'r';
+  public static final Position TOP_LEFT = new Position(0, 0);
+  public static final Position TOP_RIGHT = new Position(7, 0);
+  public static final Position BOTTOM_LEFT = new Position(0, 7);
+  public static final Position BOTTOM_RIGHT = new Position(7, 7);
 
   public Rook(final Position position, final boolean isWhite) {
     super(position, PieceType.ROOK, isWhite);
